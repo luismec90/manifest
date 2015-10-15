21aac5b2f0e692bf3947be74cce5d3cd3ccfc948
Luisfer <luismec90@gmail.com>
2015-10-15 11:43:51 -0500

    Mejoras

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
     |
     */
 
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+    use AuthenticatesAndRegistersUsers;
 
     protected $redirectPath = '/';
 
