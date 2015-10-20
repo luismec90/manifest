615a6c4663cfcd3469185b038f736d5bdee47fa0
Luisfer <luismec90@gmail.com>
2015-10-20 11:34:58 -0500

    Mejoras

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,4 +21,4 @@ Route::get('entrar', 'Auth\AuthController@getLogin');
 Route::post('entrar', 'Auth\AuthController@postLogin');
 Route::get('salir', 'Auth\AuthController@getLogout');
 
-Route::get('/api/v1/login', 'APIController@login');
+Route::post('/api/v1/login', 'APIController@login');
