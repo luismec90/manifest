915cdc455619c60671094ac9ef9594b3c14d0d10
Luisfer <luismec90@gmail.com>
2015-10-21 11:49:18 -0500

    Mejoras

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -32,7 +32,6 @@ class APIController extends Controller
             ->select("manifests.code", "suppliers.name AS supplier", "manifests.description")
             ->get();
 
-
         return ['result' => $manifests];
     }
 
